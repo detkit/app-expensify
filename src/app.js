7746e4f6f25b86e23a3d3e4d3d5b3855df8ee23a
Quang 166Dev <quangthat97@gmail.com>
2018-08-02 13:10:23 +0700

    Add Expensify style

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'normalize.css/normalize.css';
 import './style/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 
@@ -19,7 +20,7 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 store.dispatch(startSetExpenses()).then(() => {
   ReactDOM.render(jsx, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = (env) => {
   const CSSExtract = new ExtractTextPlugin('css/style.css');
 
   return {
-    entry: './src/app.js',
+    entry: ['babel-polyfill','./src/app.js'],
     output: {
         path: path.join(__dirname, 'public', 'dist'),
         filename: 'script/app.js'
